f359106d77502550fe1ae2aafcbe3c2c79020c6b
Ali Bilge <bilge@ualberta.ca>
2021-07-08 12:56:34 -0600

    General setup for task app

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from './Task'
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'July 25th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'July 26th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'July 27th at 5:30pm',
+        reminder: false,
+    },
+    {
+        id: 4,
+        text: 'Party with Friends',
+        day: 'July 28th at 6:30pm',
+        reminder: true,
+    },
+]
